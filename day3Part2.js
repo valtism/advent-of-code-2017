e2858f797f9a850f3e37c75945311c78e6ddd94e
Dan <daniel4wood@icloud.com>
2017-12-07 08:27:10 +1100

    Add unfinished implementation to day3pt2

--- day3Part2.js
+++ day3Part2.js
@@ -1,5 +1,6 @@
 function run(input) {
-    let grid = [];
+    debugger;
+    let grid = [[1]];
     let pos = {
         x: 0,
         y: 0
@@ -7,12 +8,12 @@ function run(input) {
     let n = 1;
     let direction = 'E';
     while (n < input) {
-        if(shouldTurn(pos)) {
-            turn(direction);
+        if (shouldTurn(pos)) {
+            direction = turn(direction);
         }
         pos = step(pos, direction);
-        n = sumSurrounds(pos);
-        writeToGrid(grid);
+        n = sumSurrounds(pos, grid);
+        grid = updatedGrid(pos, grid, n);
     }
     return n;
 }
@@ -20,7 +21,67 @@ function run(input) {
 function shouldTurn(pos) {
     // Special case for spiral enlargment at bottom right
     if (pos.x - 1 === -pos.y) {
-        
+        return true;
+    } else if (pos.x === -pos.y) {
+        return false;
     }
-    pos.x ==
+    // Turn at the other corners where x == y
+    return Math.abs(pos.x) === Math.abs(pos.y)
 }
+
+function turn(direction) {
+    // Turn 90deg left
+    switch (direction) {
+        case 'N':
+            return 'W';
+        case 'E':
+            return 'N';
+        case 'S':
+            return 'E';
+        case 'W':
+            return 'S';
+    }
+}
+
+function step(pos, direction) {
+    switch (direction) {
+        case 'N':
+            pos.y++;
+            break;
+        case 'E':
+            pos.x++;
+            break;
+        case 'S':
+            pos.y--;
+            break;
+        case 'W':
+            pos.x--;
+            break;
+    }
+    return pos;
+}
+
+function sumSurrounds(pos, grid) {
+    sum = 0;
+    // Sum all surrounding squares. Pos should be empty
+    for (let x = pos.x - 1; x <= pos.x + 1; x++) {
+        if (grid[x]) {
+            for (let y = pos.y - 1; y <= pos.y + 1; y++) {
+                if (grid[x][y]) {
+                    sum += grid[x][y];
+                }
+            }
+        }
+    }
+    return sum;
+}
+
+function updatedGrid(pos, grid, sum) {
+    if (!grid[pos.x]) {
+        grid[pos.x] = [];
+    }
+    grid[pos.x][pos.y] = sum;
+    return grid;
+}
+
+run(1000);
