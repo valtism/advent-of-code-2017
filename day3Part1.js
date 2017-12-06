be25c8b21a3e8bcba989f63d22e72a6f84e5d83b
Dan Wood <dwood@tofi.com.au>
2017-12-06 16:33:56 +1100

    Finished day 3 pt 1 using square number tracking.

--- day3Part1.js
+++ day3Part1.js
@@ -1,55 +1,88 @@
-// const target = 347991;
+function run(input) {
+    const roots = {
+        lower: Math.floor(Math.sqrt(input)),
+        upper: Math.ceil(Math.sqrt(input))
+    };
 
-
-function run(target) {
-    debugger;
-    let pos = {
-        x: 0,
-        y: 0
-    }
-    let direction = 'E';
-    let length = 1;
-
-    for (let i = 0; i < target; i++) {
-        move(direction, Math.floor(length), pos);
-        direction = turn(direction);
-        length = incrementLength(length);
-    }
-    console.log(x, y);
+    const coordinates = getInputCoordinates(input, roots);
+    const distance = Math.abs(coordinates.x) + Math.abs(coordinates.y);
+    return { coordinates, distance };
 }
 
-function move(direction, length, pos) {
-    switch (direction) {
-        case 'N':
-            pos.y += length;
-            break;
-        case 'E':
-            pos.x += length;
-            break;
-        case 'S':
-            pos.y -= length;
-            break;
-        case 'W':
-            pos.x -= length;
-            break;
+function getInputCoordinates(input, roots) {
+    const isUpperRight = roots.upper % 2 === 0;
+    const isCloserToLower = getIsCloserToLower(input, roots);
+    const distance = distanceToClosestSquare(input, roots);
+
+    const delta = getDeltaFromClosestSquare(isUpperRight, isCloserToLower, distance);
+    const closestRoot = isCloserToLower ? roots.lower : roots.upper;
+    const squareCoordinates = getSquareCoordinates(closestRoot);
+
+    return {
+        x: squareCoordinates.x + delta.x,
+        y: squareCoordinates.y + delta.y
+    };
+}
+
+function getDeltaFromClosestSquare(isUpperRight, isCloserToLower, distance) {
+    if (isUpperRight) {
+        if (isCloserToLower) {
+            return {
+                x: 1,
+                y: distance - 1
+            };
+        } else {
+            return {
+                x: distance,
+                y: 0
+            };
+        }
+    } else {
+        if (isCloserToLower) {
+            return {
+                x: -1,
+                y: 1 - distance
+            };
+        } else {
+            return {
+                x: -distance,
+                y: 0
+            };
+        }
     }
 }
 
-function turn(direction) {
-    switch (direction) {
-        case 'N':
-            return 'W';
-        case 'E':
-            return 'N';
-        case 'S':
-            return 'E';
-        case 'W':
-            return 'S';
+function distanceToClosestSquare(input, roots) {
+    const closerSquare = squareInputIsCloserTo(input, roots);
+    return Math.abs(input - closerSquare);
+}
+
+function getIsCloserToLower(input, roots) {
+    const closerSquare = squareInputIsCloserTo(input, roots);
+    return input > closerSquare;
+}
+
+function squareInputIsCloserTo(input, roots) {
+    const upperSquare = roots.upper ** 2;
+    const lowerSquare = roots.lower ** 2;
+    const lowerDifference = input - lowerSquare;
+    const upperDifference = upperSquare - input;
+    return upperDifference > lowerDifference ? lowerSquare : upperSquare;
+}
+
+function getSquareCoordinates(squareRoot) {
+    const isEven = squareRoot % 2 === 0;
+    if (isEven) {
+        return {
+            x: 1 - squareRoot / 2,
+            y: squareRoot / 2
+        };
+    } else {
+        return {
+            x: (squareRoot - 1) / 2,
+            y: -(squareRoot - 1) / 2
+        }
     }
 }
 
-function incrementLength(length) {
-    return length += 0.5;
-}
-
-run(12);
+run(347991);
